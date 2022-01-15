aa91929f6317b8bffa23089adb7640c7c3144d03
unblee <unblee@users.noreply.github.com>
2022-01-16 01:12:48 +0900

    Implement minimal GET request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(unused_imports)]
+use anyhow::Context as _;
+use anyhow::Result;
+use std::env;
+
+fn main() -> Result<()> {
+    let inkdrop_http_user =
+        env::var("INKDROP_HTTP_USER").context("You should set a value for $INKDROP_HTTP_USER")?;
+    let inkdrop_http_password = env::var("INKDROP_HTTP_PASSWORD")
+        .context("You should set a value for $INKDROP_HTTP_PASSWORD")?;
+    let inkdrop_http_port =
+        env::var("INKDROP_HTTP_PORT").context("You should set a value for $INKDROP_HTTP_PORT")?;
+
+    let basic_auth_value =
+        base64::encode(format!("{}:{}", inkdrop_http_user, inkdrop_http_password));
+
+    let text = ureq::get(&format!("http://localhost:{}", inkdrop_http_port))
+        .set("Authorization", &format!("Basic {}", basic_auth_value))
+        .call()
+        .context("Failed to get request to http://example.com")?
+        .into_string()?;
+    println!("{}", text);
+    Ok(())
 }
